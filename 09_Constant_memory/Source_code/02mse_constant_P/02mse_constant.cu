f8ff9f33c20c496ac71990cd97ee423297275c88
Edgar Garcia Cano <eegkno@gmail.com>
2015-03-01 14:21:51 -0500

    New practice about constant memory and random numbers.

---
+++ 09_Constant_memory/Source_code/02mse_constant_P/02mse_constant.cu
@@ -0,0 +1,98 @@
+#include "Error.h"
+#include "Vector.h"
+#include "GpuTimer.h"
+
+#define N   16
+#define BLOCK_SIZE 2
+#define STRIDE 4
+#define POW(x) (x) * (x)
+
+// constant Vectors on the GPU
+    // -:YOUR CODE HERE:-  
+
+__global__ void mseKernel( Vector<float> d_c ){
+
+    // -:YOUR CODE HERE:-   
+}
+
+
+void onDevice( Vector<float> h_a, Vector<float> h_b, Vector<float> h_c ){
+
+    // declare GPU vectors
+    // -:YOUR CODE HERE:-
+
+    // start timer
+    GpuTimer timer;
+    timer.Start();
+
+    const int ARRAY_BYTES = N * N * sizeof(float);
+
+    // allocate  memory on the GPU
+    // -:YOUR CODE HERE:-
+
+
+    //execution configuration
+    dim3 GridBlocks( 2,2 );
+    dim3 ThreadsBlocks( 2,2 );
+
+    mseKernel<<<GridBlocks,ThreadsBlocks>>>( d_c );
+    HANDLER_ERROR_MSG("kernel panic!!!");
+
+    // copy data back from the GPU to the CPU
+    // -:YOUR CODE HERE:-
+
+    // stop timer
+    timer.Stop(); 
+    
+    // print time
+    printf( "Time :  %f ms\n", timer.Elapsed() );
+
+    // free GPU memory
+    // -:YOUR CODE HERE:-
+}
+
+
+void test(){
+
+    Vector<float> h_a, h_b, h_c;
+    h_a.length = N;
+    h_b.length = N;
+    h_c.length = 4;    
+
+    h_a.elements = (float*)malloc(  h_a.length * sizeof(int) );
+    h_b.elements = (float*)malloc(  h_a.length * sizeof(int) );
+    h_c.elements = (float*)malloc(  4 * sizeof(int) );
+
+
+    int i,j = 16, k=1;
+
+    for( i = 0; i < h_a.length; i++){
+            h_a.setElement( i, k );  
+            h_b.setElement( i, j );                 
+            k++;
+            j--;            
+    }
+
+    // call device configuration
+    onDevice(h_a, h_b, h_c);
+
+     // verify that the GPU did the work we requested
+    for (int i=0; i<4; i++) {
+            printf( " [%i] = %f \n", i, h_c.getElement(i) );
+    }
+
+
+    printf("-: successful execution :-\n");
+
+    free(h_a.elements);
+    free(h_b.elements);
+    free(h_c.elements); 
+
+}
+
+int main( void ) {
+    	
+        test();
+    	return 0;
+}
+
---
+++ 09_Constant_memory/Source_code/03random_curand/03random_curand.cu
@@ -0,0 +1,65 @@
+/*
+Source: http://docs.nvidia.com/cuda/curand/index.html#topic_1_2_2
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <curand.h>
+#include "common/Error.h"
+ 
+
+
+int randomNumbersGenerator(int n){
+
+  size_t i;
+  curandGenerator_t gen;
+  float *d_data, *h_data;
+
+  const int ARRAY_BYTES = n * sizeof(float);
+
+  /* Allocate n floats on host */
+  h_data = (float *)calloc(n, sizeof(float));
+
+  /* Allocate n floats on device */
+  HANDLER_ERROR_ERR(cudaMalloc((void **)&d_data, ARRAY_BYTES));
+
+  /* Create pseudo-random number generator */
+  HANDLER_CURAND(curandCreateGenerator(&gen, 
+                                    CURAND_RNG_PSEUDO_DEFAULT));
+ 
+ 
+  /* Set seed 1234ULL = unsigned long long */
+  srand48( time(NULL) );
+  HANDLER_CURAND(curandSetPseudoRandomGeneratorSeed(gen, lrand48() ));
+
+  /* Set seed 1234ULL = unsigned long long 
+  Use this to generate the same random numbers*/
+  //HANDLER_CURAND(curandSetPseudoRandomGeneratorSeed(gen, 1234ULL));
+ 
+  /* Generate n floats on device */
+  HANDLER_CURAND(curandGenerateUniform(gen, d_data, n));
+ 
+  /* Copy device memory to host */
+  HANDLER_ERROR_ERR(cudaMemcpy(h_data, d_data, ARRAY_BYTES,
+                       cudaMemcpyDeviceToHost));
+
+
+  /* Show result */
+  for(i = 0; i < n; i++) {
+    printf("%1.4f ", h_data[i]);
+  }
+  printf("\n");
+
+  /* Cleanup */
+  HANDLER_CURAND(curandDestroyGenerator(gen));
+  HANDLER_ERROR_ERR(cudaFree(d_data));
+  free(h_data);
+  return EXIT_SUCCESS;
+
+}
+
+int main(int argc, char *argv[])
+{
+  randomNumbersGenerator( 100 );
+
+}
